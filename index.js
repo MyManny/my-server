1b8d092be58d0d9f711adfbd1fd5304ffc6a98ea
MyManny <gmail.com>
2023-08-15 20:48:51 -0700

    MADE SERVER

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require("http")
+let server =http.createServer(function (req, res) {
+    // This callback handles each incoming request.
+    //this sends the data:
+    res.write('Hello, FSD!')
+
+    //This closes the connection:
+    res.end()
+})
+server.listen(3000, function(){
+    console.log("Server is live!")
+}) //3000 is a port number
